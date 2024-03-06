c8b8331aa4e64d4e833fbfae40fd71960f7e2449
Moises Almeida <moisesssprg@gmail.com>
2024-03-06 16:31:01 -0300

    cria a pasta request para os testes e arquivo de configuração do rspec

---
+++ spec/requests/server_spec.rb
@@ -0,0 +1,80 @@
+require 'spec_helper'
+
+describe 'Roda o servidor' do
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  it 'e retorna hello world' do
+    get '/hello'
+    expect(last_response).to be_ok
+    expect(last_response.body).to eq('Hello world!')
+  end
+
+  it 'e ver os dados com sucesso' do
+    get '/tests'
+    expect(last_response).to be_ok
+    expect(last_response.content_type).to eq('application/json')
+    expected_json = {
+                      "cpf" => "048.973.170-88",
+                      "nome_paciente" => "Emilly Batista Neto",
+                      "email_paciente" => "gerald.crona@ebert-quigley.com",
+                      "data_nascimento_paciente" => "2001-03-11",
+                      "endereco_rua_paciente" => "165 Rua Rafaela",
+                      "cidade_paciente" => "Ituverava",
+                      "estado_paciente" => "Alagoas",
+                      "crm_medico" => "B000BJ20J4",
+                      "crm_medico_estado" => "PI",
+                      "nome_medico" => "Maria Luiza Pires",
+                      "email_medico" => "denna@wisozk.biz",
+                      "token_resultado_exame" => "IQCZ17",
+                      "data_exame" => "2021-08-05",
+                      "tipo_exame" => "hemácias",
+                      "limites_tipo_exame" => "45-52",
+                      "resultado_tipo_exame" => "97"
+                    }
+    expect(last_response.body).to include(expected_json.to_json)
+  end
+end
+
+
+describv 'Roda o servidor' do
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  it 'e retorna hello world' do
+    get '/hello'
+    expect(last_response).to be_ok
+    expect(last_response.body).to eq('Hello world!')
+  end
+
+  it 'e ver os dados com sucesso' do
+    get '/tests'
+    expect(last_response).to be_ok
+    expect(last_response.content_type).to eq('application/json')
+    expected_json = {
+                      "cpf" => "048.973.170-88",
+                      "nome_paciente" => "Emilly Batista Neto",
+                      "email_paciente" => "gerald.crona@ebert-quigley.com",
+                      "data_nascimento_paciente" => "2001-03-11",
+                      "endereco_rua_paciente" => "165 Rua Rafaela",
+                      "cidade_paciente" => "Ituverava",
+                      "estado_paciente" => "Alagoas",
+                      "crm_medico" => "B000BJ20J4",
+                      "crm_medico_estado" => "PI",
+                      "nome_medico" => "Maria Luiza Pires",
+                      "email_medico" => "denna@wisozk.biz",
+                      "token_resultado_exame" => "IQCZ17",
+                      "data_exame" => "2021-08-05",
+                      "tipo_exame" => "hemácias",
+                      "limites_tipo_exame" => "45-52",
+                      "resultado_tipo_exame" => "97"
+                    }
+    expect(last_response.body).to include(expected_json.to_json)
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,5 @@
+ENV['APP_ENV'] = 'test'
+
+require 'rspec'
+require 'rack/test'
+require_relative '../server'
