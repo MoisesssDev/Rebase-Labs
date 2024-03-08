c67dda08a0c61a14fe630ab7f40388cfb6826e79
Moises Almeida <moisesssprg@gmail.com>
2024-03-08 16:35:43 -0300

    melhora a legibilidade das funções javascript

--- public/main.js
+++ public/main.js
@@ -12,17 +12,37 @@ function fetchData(callback) {
       .catch(error => {
         console.error('Erro:', error);
       });
-}
+  }
   
-function generateTable(data) {
+  function generateTableBody(data) {
+    // Função para gerar o corpo da tabela com base nos dados fornecidos
+    return data.map(item => `
+      <tr>
+        <td>${item.cpf || 'N/A'}</td>
+        <td>${item.name || 'N/A'}</td>
+        <td>${item.email || 'N/A'}</td>
+        <td>${item.birthday || 'N/A'}</td>
+        <td>${item.doctor ? item.doctor.crm || 'N/A' : 'N/A'}</td>
+        <td>${item.doctor ? item.doctor.crm_state || 'N/A' : 'N/A'}</td>
+        <td>${item.doctor ? item.doctor.name || 'N/A' : 'N/A'}</td>
+        <td>${item.result_token || 'N/A'}</td>
+        <td>${item.result_date || 'N/A'}</td>
+        <td><a href="#" onclick="showDetails()">Detalhes</a></td>
+      </tr>
+    `).join('');
+  }
+  
+  function renderTable(data) {
+    // Função para renderizar a tabela no DOM
     if (!data) {
-      return '<p>Falha ao carregar dados</p>';
+      document.getElementById('table-container').innerHTML = '<p>Falha ao carregar dados</p>';
+      return;
     }
-
+  
     var title = '<h2><img src="images/note.png" alt="exames"> Resultado dos exames</h2>';
     var search = '<input type="text" id="search" placeholder="Pesquisar por token...">';
-    var msg_welcome = '<p>&#128075; Bem-vindo ao Rebase Labs!</p>'
-    var nav_table = `<div class="nav-table">${msg_welcome} <div class="nav-table-content">${title}${search}</div></div>`;
+    var msgWelcome = '<p>&#128075; Bem-vindo ao Rebase Labs! Consulte abaixo os resultados dos exames.</p>';
+    var navTable = `<div class="nav-table">${msgWelcome}<div class="nav-table-content">${title}${search}</div></div>`;
   
     var tableHeader = `
       <thead>
@@ -41,67 +61,39 @@ function generateTable(data) {
       </thead>
     `;
   
-    var tableBody = '<tbody>';
-
-    data.forEach(item => {
-        tableBody += `
-        <tr>
-            <td>${item.cpf || 'N/A'}</td>
-            <td>${item.name || 'N/A'}</td>
-            <td>${item.email || 'N/A'}</td>
-            <td>${item.birthday || 'N/A'}</td>
-            <td>${item.doctor ? item.doctor.crm || 'N/A' : 'N/A'}</td>
-            <td>${item.doctor ? item.doctor.crm_state || 'N/A' : 'N/A'}</td>
-            <td>${item.doctor ? item.doctor.name || 'N/A' : 'N/A'}</td>
-            <td>${item.result_token || 'N/A'}</td>
-            <td>${item.result_date || 'N/A'}</td>
-            <td><a href="#" onclick="showDetails()">Detalhes</a></td>
-        </tr>
-        `;
-    });
-
-    tableBody += '</tbody>';
-
-    var table = `${nav_table}<table>${tableHeader}${tableBody}</table>`;
-
+    var tableBody = `<tbody>${generateTableBody(data)}</tbody>`;
+    var table = `${navTable}<table>${tableHeader}${tableBody}</table>`;
+  
     // Adicionar o ouvinte de eventos ao input de pesquisa
     var tableContainer = document.getElementById('table-container');
     tableContainer.innerHTML = table;
-
+  
     var searchInput = document.getElementById('search');
     searchInput.addEventListener('input', function () {
-        filterTable(searchInput.value);
+      filterTable(searchInput.value);
     });
-
-    // return table;
-}
-
-function filterTable(filterText) {
-    // Obtém a referência para a tabela
-    var tabelaExames = document.querySelector('table');
+  }
   
-    // Obtém todas as linhas da tabela, excluindo a linha de cabeçalho
+  function filterTable(filterText) {
+    // Função para filtrar os resultados da tabela com base no texto fornecido
+    var tabelaExames = document.querySelector('table');
     var linhas = tabelaExames.querySelectorAll('tbody tr');
   
-    // Itera sobre as linhas e mostra ou oculta com base no filtro
     linhas.forEach(function (linha) {
       var textoLinha = linha.innerText.toLowerCase();
       var filtroLowerCase = filterText.toLowerCase();
-  
-      // Se o texto da linha contiver o texto do filtro, mostra a linha, caso contrário, oculta
       linha.style.display = textoLinha.includes(filtroLowerCase) ? '' : 'none';
     });
-}
+  }
   
-function showDetails() {
+  function showDetails() {
     console.log('Implemente a lógica para exibir detalhes dos exames aqui.');
-}
+  }
   
-document.addEventListener('DOMContentLoaded', function () {
+  document.addEventListener('DOMContentLoaded', function () {
     fetchData(function (data) {
-        console.log(data);
-        // var tableHTML = generateTable(data);
-        // document.getElementById('table-container').innerHTML = tableHTML;
-        generateTable(data);
+      console.log(data);
+      renderTable(data);
     });
-});
+  });
+  
