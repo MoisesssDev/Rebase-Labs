55d6817f51d60129ae0848dd30a51627bc9e9756
Moises Almeida <moisesssprg@gmail.com>
2024-03-12 02:57:01 -0300

    ajustando margem do link voltar

--- public/javascript/show.js
+++ public/javascript/show.js
@@ -56,20 +56,24 @@ function generateDetailsTest(data) {
       <div class="details-patient">
         <div class="info">
           <p> ${data[0].email}</p>
-          <h2>${data[0].name} - ${data[0].cpf}</h2>
+          <h2>${data[0].name}</h2>
+          <p class="cpf"><span>CPF:</span> ${data[0].cpf}</p>
         </div>
         <p><span>Data de Nascimento:</span> ${data[0].birthday}</p>
       </div>
 
       <div class="details-doctor">
-        <p><span>Médico(a):</span> ${data[0].doctor.name} - ${data[0].doctor.crm}</p>
+        <div class="info-doctor">
+          <p><span>Médico(a) responsável:</span> ${data[0].doctor.name}</p>
+          <p><span>CRM:</span> ${data[0].doctor.crm}</p>
+        </div>
         <p><span>Estado do CRM -</span> ${data[0].doctor.crm_state}</p>
       </div>
 
       <div class="details-result">
         <div class="info-result">
-          <h3>Resultados:</h3>
           <p><span>Token: </span>${data[0].result_token}</p>
+          <h3>Resultados:</h3>
         </div>
         <p><span>Data do resultado:</span> ${data[0].result_date}</p> 
       </div>
@@ -81,9 +85,9 @@ function generateDetailsTest(data) {
 
 function renderTest(data) {
   // Função para renderizar a tabela no DOM
-  var title = `<h2><img src="images/note.png" alt="exames"> Resultado do exame:</h2>`;
+  var title = `<h2><img src="images/note.png" alt="exames"> Detalhes do exame:</h2>`;
   var link_back = `<a href="/"><img src="images/left.png">Voltar</a>`;
-  var navTest = `<div class="nav-table">${link_back}<div class="nav-table-content">${title}</div></div>`;
+  var navTest = `<div class="nav-test">${link_back}<div class="nav-test-content">${title}</div></div>`;
   var details = generateDetailsTest(data);
   var table = createTable(data);
 
