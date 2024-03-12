ef67208cfcfec6eba9be760815eba6e486038c52
Moises Almeida <moisesssprg@gmail.com>
2024-03-12 01:46:38 -0300

    Adiciona estilização na pagina show

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -12,88 +12,84 @@ function fetchData(callback) {
       .catch(error => {
         console.error('Erro:', error);
       });
-  }
+}
   
-  function generateTableBody(data) {
-    // Função para gerar o corpo da tabela com base nos dados fornecidos
-    return data.map(item => `
+function generateTableBody(data) {
+  // Função para gerar o corpo da tabela com base nos dados fornecidos
+  return data.map(item => `
+    <tr>
+      <td>${item.cpf}</td>
+      <td>${item.name}</td>
+      <td>${item.email}</td>
+      <td>${item.birthday}</td>
+      <td>${item.doctor.crm}</td>
+      <td>${item.doctor.crm_state}</td>
+      <td>${item.doctor.name}</td>
+      <td>${item.result_token}</td>
+      <td>${item.result_date}</td>
+      <td><a href="/${item.result_token}">Detalhes</a></td>
+    </tr>
+  `).join('');
+}
+  
+function renderTable(data) {
+  // Função para renderizar a tabela no DOM
+  if (!data) {
+    document.getElementById('table-container').innerHTML = '<p>Não existe resultados de exames.</p>';
+    return;
+  }
+
+  var title = '<h2><img src="images/note.png" alt="exames"> Resultado dos exames</h2>';
+  var search = '<input type="text" id="search" placeholder="Pesquisar...">';
+  var msgWelcome = '<p>&#128075; Bem-vindo ao Rebase Labs! Consulte abaixo os resultados dos exames.</p>';
+  var navTable = `<div class="nav-table">${msgWelcome}<div class="nav-table-content">${title}${search}</div></div>`;
+
+  var tableHeader = `
+    <thead>
       <tr>
-        <td>${item.cpf}</td>
-        <td>${item.name}</td>
-        <td>${item.email}</td>
-        <td>${item.birthday}</td>
-        <td>${item.doctor.crm}</td>
-        <td>${item.doctor.crm_state}</td>
-        <td>${item.doctor.name}</td>
-        <td>${item.result_token}</td>
-        <td>${item.result_date}</td>
-        <td><a href="/${item.result_token}">Detalhes</a></td>
+        <th>CPF</th>
+        <th>Nome</th>
+        <th>Email</th>
+        <th>Data de Nascimento</th>
+        <th>CRM do Médico</th>
+        <th>Estado do CRM</th>
+        <th>Nome do Médico</th>
+        <th>Token do Resultado</th>
+        <th>Data do Exame</th>
+        <th>Exames</th>
       </tr>
-    `).join('');
-  }
-  
-  function renderTable(data) {
-    // Função para renderizar a tabela no DOM
-    if (!data) {
-      document.getElementById('table-container').innerHTML = '<p>Não existe resultados de exames.</p>';
-      return;
-    }
-  
-    var title = '<h2><img src="images/note.png" alt="exames"> Resultado dos exames</h2>';
-    var search = '<input type="text" id="search" placeholder="Pesquisar...">';
-    var msgWelcome = '<p>&#128075; Bem-vindo ao Rebase Labs! Consulte abaixo os resultados dos exames.</p>';
-    var navTable = `<div class="nav-table">${msgWelcome}<div class="nav-table-content">${title}${search}</div></div>`;
-  
-    var tableHeader = `
-      <thead>
-        <tr>
-          <th>CPF</th>
-          <th>Nome</th>
-          <th>Email</th>
-          <th>Data de Nascimento</th>
-          <th>CRM do Médico</th>
-          <th>Estado do CRM</th>
-          <th>Nome do Médico</th>
-          <th>Token do Resultado</th>
-          <th>Data do Exame</th>
-          <th>Exames</th>
-        </tr>
-      </thead>
-    `;
-  
-    var tableBody = `<tbody>${generateTableBody(data)}</tbody>`;
-    var table = `${navTable}<table>${tableHeader}${tableBody}</table>`;
-  
-    // Adicionar o ouvinte de eventos ao input de pesquisa
-    var tableContainer = document.getElementById('table-container');
-    tableContainer.innerHTML = table;
-  
-    var searchInput = document.getElementById('search');
-    searchInput.addEventListener('input', function () {
-      filterTable(searchInput.value);
-    });
-  }
-  
-  function filterTable(filterText) {
-    // Função para filtrar os resultados da tabela com base no texto fornecido
-    var tabelaExames = document.querySelector('table');
-    var linhas = tabelaExames.querySelectorAll('tbody tr');
-  
-    linhas.forEach(function (linha) {
-      var textoLinha = linha.innerText.toLowerCase();
-      var filtroLowerCase = filterText.toLowerCase();
-      linha.style.display = textoLinha.includes(filtroLowerCase) ? '' : 'none';
-    });
-  }
-  
-  function showDetails() {
-    console.log('Implemente a lógica para exibir detalhes dos exames aqui.');
-  }
-  
-  document.addEventListener('DOMContentLoaded', function () {
-    fetchData(function (data) {
-      console.log(data);
-      renderTable(data);
-    });
+    </thead>
+  `;
+
+  var tableBody = `<tbody>${generateTableBody(data)}</tbody>`;
+  var table = `${navTable}<table>${tableHeader}${tableBody}</table>`;
+
+  // Adicionar o ouvinte de eventos ao input de pesquisa
+  var tableContainer = document.getElementById('table-container');
+  tableContainer.innerHTML = table;
+
+  var searchInput = document.getElementById('search');
+  searchInput.addEventListener('input', function () {
+    filterTable(searchInput.value);
   });
+}
+  
+function filterTable(filterText) {
+  // Função para filtrar os resultados da tabela com base no texto fornecido
+  var tabelaExames = document.querySelector('table');
+  var linhas = tabelaExames.querySelectorAll('tbody tr');
+
+  linhas.forEach(function (linha) {
+    var textoLinha = linha.innerText.toLowerCase();
+    var filtroLowerCase = filterText.toLowerCase();
+    linha.style.display = textoLinha.includes(filtroLowerCase) ? '' : 'none';
+  });
+}
+  
+document.addEventListener('DOMContentLoaded', function () {
+  fetchData(function (data) {
+    console.log(data);
+    renderTable(data);
+  });
+});
   
--- public/javascript/show.js
+++ public/javascript/show.js
@@ -1,1 +1,97 @@
-console.log('show.js loaded!')
+function fetchData(callback) {
+  let param = window.location.pathname.split('/');
+  let token = param[1];
+
+  var apiUrl = '/api/v1/tests/' + token;
+
+  fetch(apiUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Erro na requisição: ' + response.statusText);
+      }
+      return response.json();
+    })
+    .then(data => callback(data))
+    .catch(error => {
+      console.error('Erro:', error);
+    });
+}
+
+function generateTableBody(data) {
+  // Função para gerar o corpo da tabela com base nos dados fornecidos
+  return data[0]['tests'].map(item => `
+    <tr>
+      <td>${item.type}</td>
+      <td>${item.limits}</td>
+      <td>${item.result}</td>
+    </tr>
+  `).join('');
+}
+
+function createTable(data) {
+
+  var tableHeader = `
+    <thead>
+      <tr>
+        <th>Exame</th>
+        <th>Limites</th>
+        <th>Resultado</th>
+      </tr>
+    </thead>
+  `;
+
+  var tableBody = `
+    <tbody>
+      ${generateTableBody(data)}
+    </tbody>
+  `;
+
+  return `<div id="table-container"><table>${tableHeader}${tableBody}</table></div>`;
+}
+
+
+function generateDetailsTest(data) {
+  var details = `
+    <div class="details">
+      <div class="details-patient">
+        <div class="info">
+          <p> ${data[0].email}</p>
+          <h2>${data[0].name} - ${data[0].cpf}</h2>
+        </div>
+        <p><span>Data de Nascimento:</span> ${data[0].birthday}</p>
+      </div>
+
+      <div class="details-doctor">
+        <p><span>Médico(a):</span> ${data[0].doctor.name} - ${data[0].doctor.crm}</p>
+        <p><span>Estado do CRM -</span> ${data[0].doctor.crm_state}</p>
+      </div>
+
+      <div class="details-result">
+        <div class="info-result">
+          <h3>Resultados:</h3>
+          <p><span>Token: </span>${data[0].result_token}</p>
+        </div>
+        <p><span>Data do resultado:</span> ${data[0].result_date}</p> 
+      </div>
+    </div>
+  `;
+
+  return details
+}
+
+function renderTest(data) {
+  // Função para renderizar a tabela no DOM
+  var title = `<h2><img src="images/note.png" alt="exames"> Resultado do exame:</h2>`;
+  var navTest = `<div class="nav-table"><div class="nav-table-content">${title}</div></div>`;
+  var details = generateDetailsTest(data);
+  var table = createTable(data);
+
+  document.getElementById('test-container').innerHTML = `${navTest}<div class="container">${details}${table}</div>`;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchData(data => {
+    console.log(data);
+    renderTest(data);
+  });
+});
