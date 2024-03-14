a388beba7a57f7d0da9a7eda468aedc41d1a38e8
Moises Almeida <moisesssprg@gmail.com>
2024-03-14 03:39:43 -0300

    finaliza o teste do import_csv_job

--- spec/jobs/import_csv_job_spec.rb
+++ spec/jobs/import_csv_job_spec.rb
@@ -1,15 +1,19 @@
 require 'spec_helper'
 require_relative '../../jobs/import_csv_job'
+require 'sidekiq/testing'
 
 describe '#perform' do
   it 'importa arquivo CSV com sucesso' do
-    file_path = File.join(Dir.pwd, 'spec', 'support', 'assets', 'csv', 'data_sucess.csv')
+    Sidekiq::Testing.inline!
 
-    ImportCsvJob.perform_inline(file_path)
+    file_path = File.join(Dir.pwd, 'spec', 'support', 'assets', 'csv', 'data_sucess.csv')
+    ImportCsvJob.new.perform(file_path)
+    
+    
     conn = PG.connect(dbname: 'rebaselabs', user: 'docker', password: 'docker', host: 'pgserver')
     result = find_by_token(conn, 'IQCZ17')
-
-    expect(result.ntuples).to eq(1)
+    conn.close
+    
     expect(result[0]['result_token']).to eq('IQCZ17')
     expect(result[0]['result_date']).to eq('2021-08-05')
     expect(result[0]['cpf']).to eq('048.973.170-88')
